59c64cb0a240b815522399687c8291e3c6875109
coreyjadams <coreyadams@fas.harvard.edu>
2018-01-18 14:23:06 -0600

    Update test script.

--- test/test_runner.py
+++ test/test_runner.py
@@ -8,9 +8,9 @@ def main():
     project = ProjectConfig('example_project.yml')
     project.larsoft().setup_larsoft()
     db_file = project['top_dir'] +'/work/' + project['name'] + '.db'
-    a = JobRunner(project = project, stage=project.stage('generation'), db_util = DBUtil(db_file))
+    a = JobRunner(project = project, stage=project.stage('generation'))
     a.prepare_job()
-    a.run_job()
+    a.run_job(db_util = DBUtil(db_file))
     return
 
 if __name__ == '__main__':
