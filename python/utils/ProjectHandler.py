4b978393f688b67f8fea1d223bc357c373cb7915
coreyjadams <coreyadams@fas.harvard.edu>
2018-01-31 17:12:46 -0600

    Another bug fix 3

--- python/utils/ProjectHandler.py
+++ python/utils/ProjectHandler.py
@@ -345,17 +345,17 @@ class ProjectHandler(object):
 
         # Next, count the events declared to the database for this stage:
         n_ana_events = project_db.count_events(
-            dataset=dataset=stage.output_dataset(),
+            dataset=stage.output_dataset(),
             stage=stage.name, ftype=1, status=0)
         n_out_events = project_db.count_events(
-            dataset=dataset=stage.output_dataset(),
+            dataset=stage.output_dataset(),
             stage=stage.name, ftype=0, status=0)
 
         n_ana_files = project_db.count_events(
-            dataset=dataset=stage.output_dataset(),
+            dataset=stage.output_dataset(),
             stage=stage.name, ftype=1, status=0)
         n_out_files = project_db.count_events(
-            dataset=dataset=stage.output_dataset(),
+            dataset=stage.output_dataset(),
             stage=stage.name, ftype=0, status=0)
 
         print('Report for stage {0}: ".format(stage.name)')
