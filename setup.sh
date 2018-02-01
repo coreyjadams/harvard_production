b1b9ab381c74a8f749fa4089c034379ae16c8bb4
Corey Adams <coreyjadams@gmail.com>
2018-02-01 12:36:41 -0500

    make printout better.

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=${HARVARD_PRODUCTION_TOPDIR}/python:$PYTHONPATH
 export PATH=${HARVARD_PRODUCTION_TOPDIR}/bin:$PATH
 
 echo "To submit jobs, use the syntax "
-echo "  production.py [config.yml] [-s stage] --[clean | submit | check | status]"
+echo "  production.py -y config.yml [-s stage] --[clean | submit | check | status]"
