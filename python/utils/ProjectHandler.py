e19058e274e1e2fbfe8ca97907a90645a8f1e1a2
coreyjadams <coreyadams@fas.harvard.edu>
2018-02-26 14:22:10 -0600

    forgot a comma

--- python/utils/ProjectHandler.py
+++ python/utils/ProjectHandler.py
@@ -353,20 +353,20 @@ class ProjectHandler(object):
         # Next, count the events declared to the database for this stage:
         n_ana_events = dataset_reader.sum(
             dataset=stage.output_dataset(),
-            target='nevents'
+            target='nevents',
             type=1)
         n_out_events = dataset_reader.sum(
             dataset=stage.output_dataset(),
-            target='nevents'
+            target='nevents',
             type=0)
 
         n_ana_files = dataset_reader.sum(
             dataset=stage.output_dataset(),
-            target='nevents'
+            target='nevents',
             type=1)
         n_out_files = dataset_reader.sum(
             dataset=stage.output_dataset(),
-            target='nevents'
+            target='nevents',
             type=0)
 
         print('Report for stage {0}: '.format(stage.name))
