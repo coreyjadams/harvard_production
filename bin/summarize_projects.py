cef31e0f6085a283cc4f9b8c27a51b82416f62d2
coreyjadams <coreyadams@fas.harvard.edu>
2018-03-12 14:14:59 -0500

    Summary Table.

--- bin/summarize_projects.py
+++ bin/summarize_projects.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+import html
+
 from database import ProjectReader
 from database import DatasetReader
 
@@ -13,10 +15,54 @@ def main():
     dataset_reader = DatasetReader()
     project_reader = ProjectReader()
 
-    projects = project_reader.list_datasets()
-    print projects
+    # projects = project_reader.list_datasets()
+    projects = ("test1", "test2")
+    h = html.HTML()
 
-    pass
+    table = h.table(border='1')
+    header = table.tr
+    header.th("Project")
+    header.th("ID")
+    header.th("File Count")
+    header.th("File Count (Ana)")
+    header.th("Event Count")
+    header.th("Event Count (Ana)")
+    header.th("Disk Usage")
+    header.th("Disk Usage (Ana)")
+    header.th("Parents")
+
+    for project in projects:
+        row = table.tr
+
+        row.td("{0}".format(project))
+
+        project_id      = project_reader.dataset_ids(project)
+        row.td("{0}".format(project_id))
+
+        file_count      = dataset_reader.count_files(project, type=0)
+        row.td("{0}".format(file_count))
+
+        file_count_ana  = dataset_reader.count_files(project, type=1)
+        row.td("{0}".format(file_count_ana))
+
+        event_count     = dataset_reader.sum(project,target='nevents',type=0)
+        row.td("{0}".format(event_count))
+
+        event_count_ana = dataset_reader.sum(project,target='nevents',type=1)
+        row.td("{0}".format(event_count_ana))
+
+        disk_usage      = dataset_reader.sum(project,target='size',type=0)
+        row.td("{0}".format(disk_usage))
+
+        disk_usage_ana  = dataset_reader.sum(project,target='size',type=1)
+        row.td("{0}".format(disk_usage_ana))
+
+        parents         = project_reader.direct_parents(dataset_name=project)
+        row.td("{0}".format(parents))
+
+    with open("harvard_projects_summary.html", "w") as html_file:
+        html_file.write(str(h))
+
 
 if __name__ == '__main__':
     main()
