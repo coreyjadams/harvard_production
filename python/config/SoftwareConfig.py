18052c549b50c27fd3c8d5fd8964440e13b3368c
coreyjadams <coreyadams@fas.harvard.edu>
2018-03-12 10:03:01 -0500

    Add getitem

--- python/config/SoftwareConfig.py
+++ python/config/SoftwareConfig.py
@@ -16,8 +16,25 @@ class SoftwareConfig(object):
     """docstring for SoftwareConfig"""
     def __init__(self):
         super(SoftwareConfig, self).__init__()
-
+        self.yml_dict = dict()
 
     def setup(self, return_env=False):
         raise NotImplementedError("Required to implement the setup function.")
 
+
+
+    def __getitem__(self, key):
+        '''return objects
+
+        Returns yml parameters, and returns defaults for some parameters if they
+        are not specified in the file
+
+        Arguments:
+            key {key} -- dictionary key, typcially string
+
+        Returns:
+            object -- value for the key
+        '''
+        # List of defaults:
+
+        return self.yml_dict[key]
