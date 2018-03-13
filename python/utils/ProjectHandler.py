d72db9de4c36f718118aeadb691f9127a5d772a6
coreyjadams <coreyadams@fas.harvard.edu>
2018-03-13 10:54:46 -0500

    Starting makeup job implementation.

--- python/utils/ProjectHandler.py
+++ python/utils/ProjectHandler.py
@@ -48,27 +48,6 @@ class ProjectHandler(object):
         self.make_directory(self.stage_work_dir)
 
 
-    # def first_stage_check(self):
-    #     '''Initialize data base for input files
-
-    #     If the first stage is using a dataset for input, not 'none',
-    #     read the other data set and initialize it as input files for the first
-    #     stage in this projects database file.
-    #     '''
-
-    #     # For the first stage, only:
-    #     if self.config.stages.values()[0]['input']['dataset'] != 'none':
-    #         input_db = DBUtil(self.config.stages.values()[0]['input']['dbfile'])
-    #         # Select the files from the input dataset:
-    #         try:
-    #             self.project_db.initialize_from(input_db,
-    #                 dataset = self.config.stages.values()[0]['input']['dataset'],
-    #                 stage   = self.config.stages.values()[0]['input']['stage'])
-    #         except:
-    #             print('Not initilizing first stage database.')
-
-    #     return
-
 
     def act(self):
         if self.action == 'submit':
@@ -379,6 +358,18 @@ class ProjectHandler(object):
         print('  Completed {n_out} events of {target} specified, across {n_out_files} output files.'.format(
             n_out = n_out_events, target = total_out_events, n_out_files=n_out_files))
 
+
+        #Calculate how many makeup jobs to run
+        # Look at:
+        # how many events are supposed to be there
+        # how many events were produced, over how many files
+        # how many files per job are consumed (if using an input)
+
+
+
+        # How many events were produced over how many files?
+
+
         # # Check if there are still jobs running for this stage
         # n_running_jobs = self.n_running_jobs()
         # if n_running_jobs != 0:
@@ -409,6 +400,12 @@ class ProjectHandler(object):
 
         If no jobs are running, submit jobs to complete the previous stage of running.
         '''
+
+        # First, make sure there are no jobs running for the current submission
+        # of this project
+
+        # First,
+        n_makeup_jobs
 
         # Makeup command requires a check stage command first
         print ('Submission of makeup jobs is not implemented yet.')
