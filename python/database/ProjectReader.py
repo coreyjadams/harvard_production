c9a3fe9411d31a1ad1f04842018f8fa1191b81e0
coreyjadams <coreyadams@fas.harvard.edu>
2018-04-02 14:21:23 -0500

    Add function to verify parents exist

--- python/database/ProjectReader.py
+++ python/database/ProjectReader.py
@@ -40,6 +40,26 @@ class ProjectReader(ReaderBase):
             conn.execute(dataset_list_sql)
             return conn.fetchall()
 
+    def has_parents(self, dataset):
+        '''Return True if this dataset has a parent, and therefore a consumption table
+        '''
+
+        parent_existence_sql = '''
+            SELECT id
+            FROM dataset_master_index
+            WHERE output=%s
+        '''
+
+        dataset_id = (self.dataset_ids(dataset),)
+
+        with self.connect() as conn:
+            conn.execute(parent_existence_sql, dataset_id)
+            res = conn.fetchall()
+            if len(res) > 0:
+                return True
+
+        return False
+
     def dataset_ids(self, datasets):
         '''Return a list of primary keys for the datasets specified
 
@@ -118,10 +138,8 @@ class ProjectReader(ReaderBase):
         '''
 
         with self.connect() as conn:
-            print dataset_id
             conn.execute(parent_lookup_sql, (dataset_id,))
             parent_ids = conn.fetchall()[0]
-            print parent_ids
 
         if return_mode == 0:
             return parent_ids
@@ -131,14 +149,10 @@ class ProjectReader(ReaderBase):
                 SELECT dataset from dataset_master_index
                 WHERE id=%s
             '''
-            parent_names = []
-            print type(parent_ids)
             with self.connect() as conn:
-                conn.execute(parent_name_sql, (136,))
-                print conn.fetchone()
+                conn.executemany(parent_name_sql, parent_ids)
+                return conn.fetchall()
 
-                parent_names.append(conn.fetchone())
-            return parent_names
 
     def direct_daughters(self, dataset_id=None, dataset_name=None):
         '''Return the direct daughters of this dataset
