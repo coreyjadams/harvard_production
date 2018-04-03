a0aac1093bc7e676df472ea455a23c4ecb1bdeea
coreyjadams <coreyadams@fas.harvard.edu>
2018-04-03 14:51:26 -0500

    Add statistics option.

--- bin/production.py
+++ bin/production.py
@@ -38,7 +38,11 @@ def main():
                          dest='action',
                          const='makeup',
                          help="Submit makeup jobs for a stage")
-
+    actions.add_argument('--statistics',
+                         action='store_const',
+                         dest='action',
+                         const='makeup',
+                         help="Submit makeup jobs for a stage")
 
     args = parser.parse_args()
 
