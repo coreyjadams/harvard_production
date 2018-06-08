7f34619f5cf025a39666ab63faff24a9439e4fd7
coreyjadams <coreyadams@fas.harvard.edu>
2018-06-08 06:44:02 -0500

    Class name fix

--- src/utils/ICRunner.py
+++ src/utils/ICRunner.py
@@ -8,13 +8,13 @@ from database import ProjectUtils, DatasetUtils
 
 from JobRunner import JobRunner, cd
 
-class ArtNextRunner(JobRunner):
+class ICRunner(JobRunner):
     """
     Class for running a single larsoft job.  Can use multiple files
     at once and handle larsoft commands
     """
     def __init__(self, project, stage):
-        super(ArtNextRunner, self).__init__(project, stage)
+        super(ICRunner, self).__init__(project, stage)
         self.project = project
         self.stage = stage
 
