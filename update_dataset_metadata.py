6aef8c4f4d5cafe1ee62a42e0cdbb3ed644b70b5
coreyjadams <coreyadams@fas.harvard.edu>
2018-06-27 16:43:26 -0500

    Testing an update

--- update_dataset_metadata.py
+++ update_dataset_metadata.py
@@ -15,7 +15,47 @@ def main():
     projects = proj_utils.list_datasets()
 
     for project in projects:
-        print project
+        name = project[0]
+
+        if 'bnb' in name:
+            experiment = 'uboone'
+        elif 'ubxsec' in name:
+            experiment = 'uboone'
+        elif 'NEW' in name:
+            experiment = 'next'
+        elif 'sbnd' in name:
+            experiment = 'sbnd'
+
+        if experiment == 'uboone':
+            project = 'mcc8'
+            subproject = 'none'
+            if 'marco' in name:
+                subproject = 'ubxsec'
+
+        elif experiment == 'sbnd':
+            project = 'dl_samples'
+            if 'larsoft' in name:
+                subproject = 'larsoft'
+            if 'larcv' in name:
+                subproject = 'larcv'
+            if 'anatree' in name:
+                subproject = 'anatree'
+
+        elif experiment == 'next':
+            project = 'calibration'
+            if 'detsim' in name:
+                subproject = 'detsim'
+            if 'rwf' in name:
+                subproject = 'rwf'
+            if 'nexus' in name:
+                subproject = 'nexus'
+            if 'pmaps' in name:
+                subproject = 'pmaps'
+
+        print name
+        print '  ' + experiment
+        print '  ' + project
+        print '  ' + subproject
 
 
 
