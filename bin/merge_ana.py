e289852fd3421b7e4cf316638b77edba31dacac7
coreyjadams <coreyadams@fas.harvard.edu>
2018-07-13 09:42:34 -0500

    Add check against missing executable

--- bin/merge_ana.py
+++ bin/merge_ana.py
@@ -10,6 +10,35 @@ from collections import OrderedDict
 from database import ProjectReader
 from database import DatasetReader
 
+#
+# This function below, 'which', is from the stackoverflow link.  It
+# is used here to verify that the script passed to the merger is
+# in fact an available executable.
+#
+# As of python 3.3, there is shutil.which, and that has this functionality
+# It's implemented here to allow python 2.* compatibility.
+#
+# https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+#
+#
+
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
 def main():
 
 
@@ -57,6 +86,10 @@ def main():
     return
 
 def merge(project, output_directory, file_splitting_dict, script='hadd', _type=1):
+
+    # Verfiy the exectuable will work:
+    if which(script) is None:
+        raise Exception("Executable script {} either doesn't exist on the $PATH or is not executable.".format(script))
 
     # Get the list of projects, number of files (ana and non-ana), number of
     # events (ana and non-ana), and disk usage, and parents
