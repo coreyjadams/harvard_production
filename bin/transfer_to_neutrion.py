d25c5f509f600197034f2f0fc8326fea8081606e
coreyjadams <coreyadams@fas.harvard.edu>
2018-08-02 11:27:59 -0500

    Add cd class.

--- bin/transfer_to_neutrion.py
+++ bin/transfer_to_neutrion.py
@@ -15,6 +15,21 @@ from utils import ProjectHandler
 
 # If it is not finished, it submits the jobs.
 
+class cd:
+    """Context manager for changing the current working directory
+
+    Taken from: https://stackoverflow.com/questions/431684/how-do-i-cd-in-python/13197763#13197763
+    """
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
 
 def move_files_to_neutrino(yml_name, stage):
 
