6e20c07edc64b858d3dfc471923aedd239f8de3e
sebprince <44129829+sebprince@users.noreply.github.com>
2018-10-15 13:00:31 -0400

    Change location of mysqldb
    The file mysqldb now lives in guenette home folder.

--- python/database/ReaderBase.py
+++ python/database/ReaderBase.py
@@ -7,7 +7,7 @@ from connect_db import read_connection
 class ReaderBase(object):
 
     def __init__(self):
-        self._password_file = "/n/home00/cadams/mysqldb"
+        self._password_file = "/n/home03/guenette/ProductionFile/mysqldb"
 
     def connect(self):
         return read_connection(self._password_file)
